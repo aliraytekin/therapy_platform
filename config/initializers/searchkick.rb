026f3b0c6eeb824bb027df2072bbe6a3a25d0df8
Ali Aytekin <aliraytekin@gmail.com>
2025-09-21 10:47:35 +0200

    Change searchkick initializer

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,10 +1,19 @@
-Searchkick.client = Elasticsearch::Client.new(
-  url: "https://localhost:9200",
-  user: "elastic",
-  password: "X4aSPvfXdmMYsZs-3L+h",
-  transport_options: {
-    ssl: {
-      ca_file: Rails.root.join("config", "certs", "http_ca.crt").to_s
-    }
-  }
-)
+Searchkick.client =
+  if Rails.env.production?
+    Elasticsearch::Client.new(
+      url: ENV.fetch("BONSAI"),
+      transport_options: { request: { timeout: 5 } }
+    )
+  else
+    Elasticsearch::Client.new(
+      url: "https://localhost:9200",
+      user: "elastic",
+      password: "X4aSPvfXdmMYsZs-3L+h",
+      transport_options: {
+        request: { timeout: 5 },
+        ssl: {
+          ca_file: Rails.root.join("config", "certs", "http_ca.crt").to_s
+        }
+      }
+    )
+  end
