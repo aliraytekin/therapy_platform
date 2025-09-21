e24525c6f3f09fe0145c2f603297cfafe232a85a
Ali Aytekin <aliraytekin@gmail.com>
2025-09-21 11:00:27 +0200

    Searchkick

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,19 +1,5 @@
 Searchkick.client =
-  if Rails.env.production?
-    Elasticsearch::Client.new(
-      url: ENV.fetch("BONSAI"),
-      transport_options: { request: { timeout: 5 } }
-    )
-  else
-    Elasticsearch::Client.new(
-      url: "https://localhost:9200",
-      user: "elastic",
-      password: "X4aSPvfXdmMYsZs-3L+h",
-      transport_options: {
-        request: { timeout: 5 },
-        ssl: {
-          ca_file: Rails.root.join("config", "certs", "http_ca.crt").to_s
-        }
-      }
-    )
-  end
+  Elasticsearch::Client.new(
+    url: ENV.fetch("BONSAI_URL"),
+    transport_options: { request: { timeout: 5 } }
+  )
