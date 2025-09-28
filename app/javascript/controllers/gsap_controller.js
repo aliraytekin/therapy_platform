64a6375a933e22f5b51899a6d04cf8fce6d2bb4d
Ali Aytekin <aliraytekin@gmail.com>
2025-09-28 15:07:07 +0200

    GSAP transitions
    - Added transitions to text and images in the therapy_type section
    - Fixed several bugs

--- app/javascript/controllers/gsap_controller.js
+++ app/javascript/controllers/gsap_controller.js
@@ -5,13 +5,12 @@ import { ScrollTrigger } from "gsap/ScrollTrigger"
 gsap.registerPlugin(ScrollTrigger)
 
 export default class extends Controller {
-  static targets = ["slides"]
-
   connect() {
+    const slides = document.querySelector(".slides")
     const contents = gsap.utils.toArray(".content")
     const container = document.querySelector("#therapy_type")
 
-    gsap.to(this.slidesTarget, {
+    const mainAnim = gsap.to(slides, {
       xPercent: -100 * (contents.length - 1),
       ease: "none",
       scrollTrigger: {
@@ -21,6 +20,62 @@ export default class extends Controller {
         scrub: 1,
         end: () => "+=" + container.offsetWidth,
       }
+    })
+
+    contents.forEach(content => {
+      const img = content.querySelector(".content-right")
+
+      gsap.fromTo(img,
+        { filter: "blur(8px)", opacity: 0.6 },
+        {
+          filter: "blur(0px)",
+          opacity: 1,
+          duration: 1,
+          scrollTrigger: {
+            trigger: content,
+            containerAnimation: mainAnim,
+            start: "30% center",
+            horizontal: true,
+            toggleActions: "play reverse play reverse"
+          }
+        })
+    })
+
+    contents.forEach(content => {
+      const img = content.querySelector(".content-right")
+
+      gsap.to(img, {
+        scale: 1.02,
+        duration: 2,
+        yoyo: true,
+        repeat: 1,
+        ease: "sine.inOut",
+        paused: true,
+        scrollTrigger: {
+          trigger: content,
+          containerAnimation: mainAnim,
+          start: "30% center",
+          toggleActions: "play none none reverse"
+        }
+      })
+    })
+
+    contents.forEach((content, i) => {
+      if (i === 0) return;
+
+      const text = content.querySelector(".content-left")
+
+      gsap.from(text, {
+        x: -50,
+        opacity: 0,
+        duration: 0.5,
+        scrollTrigger: {
+          trigger: content,
+          containerAnimation: mainAnim,
+          start: "left center",
+          toggleActions: "play none none reverse"
+        }
+      });
     })
   }
 }
